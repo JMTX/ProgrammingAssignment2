61afb7d43e3be4544f13bc6528ab887ef5895beb
JMTX <jeanmarc.tacchella@gmail.com>
2020-05-05 07:39:11 +0200

    add some comments to describe the three functions "makeCacheMatrix", "cacheSolve" and "is_sq_matrix"

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Below are 2 functions that are used to create a special object that stores a matrix and caches its inverse.
+##A third function was added to check if a matrix is squared with 0 NaN values
 
-## Write a short comment describing this function
+##The first function makeCacheMatrix creates a special "matrix", which is really a list containing a function to:
+##1- set the value of the matrix
+##2- get the value of the matrix
+##3- set the value of the inverse of the matrix
+##4- get the value of the inverse of the matrix
 
+## this list is created only if the arg x is a squared non NaN matrix else the function returns NULL.
+##To check this fact, the third function "is_sq_matrix" is used.
 makeCacheMatrix <- function(x = matrix()) {
     if (is_sq_matrix(x)){
         inv_x <- NULL
@@ -24,7 +30,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix" created with the above function.
+## However, it first checks to see if the inverse has already been calculated.
+## If so, it gets the inverse from the cache and skips the computation. 
+##Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via the set_inv function
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -39,11 +48,13 @@ cacheSolve <- function(x, ...) {
     inv_x
 }
 
-
+##function to check if an arg x is a squared non NaN matrix. 
+##Return the boolean value r_value: TRUE if x is a squared matrix with 0 Nan values
+##or FALSE if not.
+##This function check also if the determinant of the matrix is equal to 0 but
+## only print a warning in this case to say that the matrix is not invertible
 is_sq_matrix<-function(x){
-    #function to check if an arg is a square non NaN matrix. 
-    #Return the boolean value r_value: TRUE if x is a square matrix with non Nan values
-    #or FALSE if not.
+    
     r_value = FALSE
     eps<-10**(-9)
     #check if the arg is a matrix
